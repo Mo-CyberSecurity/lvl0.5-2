d697732d4237c895757e49f52947664b8186e97c
Momavi <cool.ru1960@gmail.com>
2022-04-25 13:09:20 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express"),
+  app = express(),
+  path = require("path");
+
+const port = process.env.YOUR_PORT || process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.append("Cake", "flag{cake_is_lie}");
+  res.sendFile(__dirname + "/public/" + "index.html");
+});
+
+app.listen(port, () => {
+  console.log(`Start on port:${port}`);
+});
+
+app.use(express.static(path.join(__dirname, "public")));
